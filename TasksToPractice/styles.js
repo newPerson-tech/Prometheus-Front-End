1e18e26ae6a38717346521680f67c0fc4c9db0df
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-12-03 16:31:31 +0000

    ..

---
+++ TasksToPractice/styles.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container');
+container.style.textAlign = 'center';
+
+const image = document.querySelector('img');
+image.style.width = '150px';
+image.style.borderRadius = '50%';
+
